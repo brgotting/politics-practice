da4b9d59377b901da47ae5485a1f80177ba1bfc2
Blake Gotting, Cole Marsteller and Devin Sweeting <cole@>
2018-10-18 14:33:14 -0700

    add branching, give user ability to choose age range

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-
+  $("form#ageSelector").submit(function() {
+    event.preventDefault();
+    var ageRange = $("input:radio[name=ageRange]:checked").val();
+    if (ageRange === "18-35") {
+      result = "18-35";
+    } else if (ageRange === "35+") {
+      result = "35+";
+    }
+    $("#output").text(result);
+  });
 });
